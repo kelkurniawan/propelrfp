d493597684562bd857d26d3bb47a1175cf68f4ac
kelkurniawan <kurniawan.michael24@gmail.com>
2026-05-16 12:47:58 +0700

    feat: add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         {children}
         <Toaster richColors />
+        <Analytics />
       </body>
     </html>
   );
